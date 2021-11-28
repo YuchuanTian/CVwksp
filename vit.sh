38b719d180067181deb0f925f9af5a0c9afe3643
Yuchuan Tian <45408600+YuchuanTian@users.noreply.github.com>
2021-11-28 12:57:03 +0800

    Delete unnecessary shell script

--- vit.sh
+++
@@ -1,9 +0,0 @@
-models=( 'vit_base_patch16_224' 'vit_base_patch32_224' 'vit_large_patch16_224' 'vit_large_patch32_224' 'vit_small_patch16_224' 'vit_small_patch32_224' 'tnt_b_patch16_224' 'tnt_s_patch16_224' )
-lrs=( 0.01 0.001 0.0001 )
-for i in ${models[*]}
-do
-for j in ${lrs[*]}
-do
-python train.py --model ${i} --lr ${j} --gpu 3
-done
-done

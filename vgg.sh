9dc6735a2f973634595eb63ec9c67cfd9e70ea03
YuchuanTian <tony_2605@126.com>
2021-11-28 05:30:27 +0000

    Delete unnecessary shell script

--- vgg.sh
+++
@@ -1,9 +0,0 @@
-models=( 'vgg11' 'vgg11_bn' 'vgg13' 'vgg13_bn' 'vgg16' 'vgg16_bn' 'vgg19' 'vgg19_bn' )
-lrs=( 0.01 0.001 0.0001 )
-for i in ${models[*]}
-do
-for j in ${lrs[*]}
-do
-python train.py --model ${i} --lr ${j} --gpu 2
-done
-done
